6cf9e161fcaa8d132dac036366dc40deadbac4c4
Tom B <me@iaero.me>
2014-08-01 15:35:19 +0100

    Adding configuration files and project skeleton

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+#   _                                                     
+#  (_)    A self-hosted ShareX image serving solution.    
+#   _ _ __ ___   __ _  __ _  ___  ___  ___ _ ____   _____ 
+#  | | '_ ` _ \ / _` |/ _` |/ _ \/ __|/ _ \ '__\ \ / / _ \
+#  | | | | | | | (_| | (_| |  __/\__ \  __/ |   \ V /  __/
+#  |_|_| |_| |_|\__,_|\__, |\___||___/\___|_|    \_/ \___|
+#                      __/ |                              
+#                     |___/   created by github.com/aerouk
+
+/* More information on all these values can be found on the wiki page. */
+/* https://github.com/aerouk/imageserve/wiki/Configuration */
+
+define('RAW_IMAGE', true);
+
+define('PASSKEY', "password goes here");

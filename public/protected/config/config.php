09a905e7d56f715ba505c812a8ffc2254e76e70c
Tom B <me@iaero.me>
2015-11-11 10:27:55 +0000

    Fix documentation...

--- public/protected/config/config.php
+++ public/protected/config/config.php
@@ -17,8 +17,8 @@ define('RAW_IMAGE_LINK', false);
 
 define('TWITTER_CARDS', true);
 
-// If you're using this, make sure to put a forward slash afterwards.
-// E.g. "imageserve/" not "imageserve"
+// If you're using this, make sure to put a forward slash before.
+// E.g. "/imageserve" not "imageserve"
 define('IMAGESERVE_DIR', "");
 define('TWITTER_HANDLE', "@aerouk_");
 
